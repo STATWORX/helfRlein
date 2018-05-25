8ed9c7eb0b462a51da0b2f1ca60cfa9380027d59
Jakob Gepp <jakob.gepp@statworx.de>
2018-05-26 00:48:12 +0200

    rename folder into statworx, add pdf

---
+++ statworx/R/statworx.R
@@ -0,0 +1,12 @@
+#' This package contains some helper functions.
+#'
+#' 
+#' There is no other purpose to the whole package but to collect and distribute
+#' some of the most used functions or helper functions which are used over and
+#' over again.
+#'
+#' @name statworx-package
+#' @aliases statworx-package statworx helperfunctions
+#' @docType package
+#' @author Jakob Gepp <jakob.gepp@@statworx.com>
+NULL
