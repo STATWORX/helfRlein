b20cab1085b3be429df4f2c235225ef9b0fddeaf
Jakob Gepp <jakob.gepp@statworx.de>
2018-07-09 22:53:55 +0200

    changing from test_function to test-function

--- tests/testthat/test-tests.R
+++ tests/testthat/test-tests.R
@@ -7,17 +7,23 @@ test_that("if for each function there is an testfile namend 'test_xxx'", {
   
   # get functions and tests
   function_files <- list.files("../../R/")
-  test_files <- gsub("test_", "", list.files("../../tests/testthat/"))
+  test_files <- gsub("test-", "", list.files("../../tests/testthat/"))
   
   # remove exceptions from tests
   package_files <- c("tests.R", "helfRlein.R")
   exceptions <- c("burglr.R", "clean_gc.R", "get_network.R",
-                  "multiplot.R", "dive.R", "helfRlein.R")
+                  "multiplot.R", "dive.R")
+  if (length(intersect(exceptions, test_files)) != 0) {
+    warning("there are exceptions for files that have a test")
+  }
+  
   function_files <- setdiff(function_files, c(exceptions, package_files))
   
   # check if all fucntions have a test
   if (length(exceptions) != 0) {
-    warning("there are still test exceptions!")
+    warning(paste0("there are still ", length(exceptions),
+                   " test exceptions: ",
+                   paste0(exceptions, collapse = ", ")))
   }
   expect_equal(all(function_files %in% test_files), TRUE)
   
