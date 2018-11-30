176f64cefc901413509640b431d611edd4744b05
Jakob Gepp <jakob.gepp@statworx.de>
2018-11-30 11:52:14 +0100

    adjust interserct2 for lists and vectors

--- tests/testthat/test-intersect2.R
+++ tests/testthat/test-intersect2.R
@@ -1,11 +1,6 @@
 context("check intersect2")
 
 
-test_that("there are at least two inputs", {
-  expect_error(intersect2(c(1:4)),
-               "cannot evaluate intersection fewer than 2 arguments")
-})
-
 test_that("if the input has diffrent formats", {
   expect_warning(intersect2(c(1:3), c(1:4), c("2", "1")),
                  "different input types will be converted")
@@ -14,6 +9,14 @@ test_that("if the input has diffrent formats", {
 test_that("the output is right for same input types", {
   expect_equal(intersect2(c(1:3), c(1:4), c(1:2)),
                c(1,2))
+})
+
+test_that("list and vector input", {
+  expect_warning(tmp1 <- intersect2(list(c(1:3), c(1:4)),
+                     list(c(1:2),c(1:3)),
+                     c(1:2)),
+                 "different input types will be converted")
+  expect_equal(tmp1, c(1,2))
 })
 
 
