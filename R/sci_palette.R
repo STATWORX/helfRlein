108bb76caafd69dc16d6d5ee8e1a76554480536c
Martin Albers <martin.albers@statworx.com>
2018-12-10 20:41:14 +0100

    Translate to englisch color names  and add new color for red

--- R/sci_palette.R
+++ R/sci_palette.R
@@ -19,15 +19,16 @@
 #' 
 sci_palette <- function(number = NULL) {
   
-  out <- c(hauptfarbe = grDevices::rgb(1, 56, 72, maxColorValue = 255),
-           akzent_1   = grDevices::rgb(0, 133, 175, maxColorValue = 255),
-           akzent_2   = grDevices::rgb(0, 163, 120, maxColorValue = 255),
-           akzent_3   = grDevices::rgb(9, 85, 127, maxColorValue = 255),
+  out <- c(main_color = grDevices::rgb(1, 56, 72, maxColorValue = 255),
+           accent_color_1   = grDevices::rgb(0, 133, 175, maxColorValue = 255),
+           accent_color_2   = grDevices::rgb(0, 163, 120, maxColorValue = 255),
+           accent_color_3   = grDevices::rgb(9, 85, 127, maxColorValue = 255),
            highlight  = grDevices::rgb(255, 128, 0, maxColorValue = 255),
-           schwarz    = grDevices::rgb(0, 0, 0, maxColorValue = 255),
-           fliestext  = grDevices::rgb(105, 105, 105, maxColorValue = 255),
-           grau_2     = grDevices::rgb(217, 217, 217, maxColorValue = 255),
-           hellgrau   = grDevices::rgb(248, 248, 248, maxColorValue = 255))
+           black    = grDevices::rgb(0, 0, 0, maxColorValue = 255),
+           text  = grDevices::rgb(105, 105, 105, maxColorValue = 255),
+           grey_2     = grDevices::rgb(217, 217, 217, maxColorValue = 255),
+           light_gray   = grDevices::rgb(248, 248, 248, maxColorValue = 255),
+           special = grDevices::rgb(198, 47, 75, maxColorValue = 255))
   
   # check input number
   if (is.null(number)) {
