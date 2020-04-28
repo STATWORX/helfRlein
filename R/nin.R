62e33499d066c62eaf8dfd3ec9f63a5077a6ae39
Jakob Gepp <jakob.gepp@statworx.de>
2020-04-28 12:17:16 +0200

    rewriting the function to fit ducumentation check

--- R/nin.R
+++ R/nin.R
@@ -1,12 +1,20 @@
 #' @title 'not in' operator
 #'
-#' @description 
+#' @description
 #'   This negates the '%in%' operator.
+#'
+#' @param x vector or NULL: the values to be matched. Long vectors are
+#'    supported.
+#' @param table vector or NULL: the values to be matched against. Long vectors
+#'    are not supported.
 #'
 #' @return a boolean vector
 #' @export
-#'
+#' @seealso \link[base]{match}
 #' @examples
 #' c(1,2,3,4) %nin% c(1,2,5)
-#' 
-`%nin%` <- Negate(`%in%`)
+#' # [1] FALSE FALSE  TRUE  TRUE
+#'
+`%nin%` <- function(x, table) {
+  !match(x, table, nomatch = 0) > 0
+}
