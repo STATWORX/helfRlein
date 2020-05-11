e584a6f875df38df1f9c986202a133df0b6d8ec0
lukasfeick-sw <lukas.feick@statworx.com>
2020-05-11 11:07:33 +0200

    Refine condition handling and warning messages in save_rds_archive

--- R/save_rds_archive.R
+++ R/save_rds_archive.R
@@ -26,7 +26,7 @@
 #'   archiving and modification date. Set to \code{TRUE} if you want to keep
 #'   several versions of files archived on a single day. See details.
 #' @param archive_dir_path Character - if desired, path to a dedicated archive
-#'   (sub-)directory (relative to the directory of \code{file}) where the
+#'   (sub-)directory (\emph{relative} to the directory of \code{file}!) where the
 #'   archived file will be saved. Will be created if it does not yet exist.
 #'   Defaults to \code{NULL}.
 #' @param ... Additional arguments passed along to \code{\link[base]{saveRDS}}
@@ -78,109 +78,116 @@ save_rds_archive <- function(object,
                              with_time = FALSE,
                              archive_dir_path = NULL,
                              ...) {
-
+  
   if (file == "" | !"character" %in% class(file)) {
     stop("'file' must be a non-empty character string")
   }
-
+  
   if (!is.null(archive_dir_path) && archive_dir_path == "") {
     stop("must supply a directory name to 'archive_dir_path' if not NULL")
   }
-
+  
   if (!is.logical(archive)) {
     archive <- TRUE
     warning("'archive' is not set to a boolean - will use default: ", archive)
   }
-
+  
   if (!is.logical(with_time)) {
     with_time <- FALSE
     warning("'with_time' is not set to a boolean - will use default: ",
             with_time)
   }
-
+  
   # IF ARCHIVE == TRUE --------------------------------------------------------
-
+  
   if (archive) {
-
+    
     # check if file exists
     if (file.exists(file)) {
-
+      
       archived_file <- create_archived_file(file = file,
                                             last_modified = last_modified,
                                             with_time = with_time)
-
+      
       if (!is.null(archive_dir_path)) {
-
+        
         # get parent directory
         dname <- dirname(file)
-
+        
         # create archive dir if it does not already exist
         if (!dir.exists(file.path(dname, archive_dir_path))) {
           dir.create(file.path(dname, archive_dir_path), recursive = TRUE)
           message("Created missing archive directory ",
                   sQuote(archive_dir_path))
         }
-
+        
         # change path of archived file into 'archive' folder
         archived_file <- file.path(dirname(archived_file),
                                    archive_dir_path,
                                    basename(archived_file))
-
+        
         # copy (rather than rename) file
         # rename sometimes does not work if the directory itself is changed
-        # save return value of the file.copy function
-        # set "overwrite" to TRUE so an existing copy is overwritten
-        # (see details)
-
+        # save return value of the file.copy function and wrap in tryCatch
+        # set "overwrite" to T so an existing copy is overwritten (see details)
+        
         if (file.exists(archived_file)) {
           warning("Archived copy already exists - will overwrite!")
         }
-
-        temp <- file.copy(from = file,
-                          to = archived_file,
-                          overwrite = TRUE)
-
+        
+        temp <- tryCatch({file.copy(from = file,
+                                    to = archived_file,
+                                    overwrite = TRUE)
+        },
+        warning = function(e) {
+          stop(e)
+        })
+        
       } else {
-
+        
         if (file.exists(archived_file)) {
           warning("Archived copy already exists - will overwrite!")
         }
 
         # rename existing file with the new name
         # save return value of the file.rename function
-        # (returns TRUE if successful)
-        temp <- file.rename(from = file,
-                            to = archived_file)
-
+        # (returns TRUE if successful) and wrap in tryCatch
+        temp <- tryCatch({file.rename(from = file,
+                                      to = archived_file)
+        },
+        warning = function(e) {
+          stop(e)
+        })
+        
       }
-
+      
       # check return value and if archived file exists
       if (temp & file.exists(archived_file)) {
         # then save new file under specified name
         saveRDS(object = object, file = file, ...)
       }
-
-
+      
+      
     } else {
-
-      warning("Nothing to overwrite - will use regular saveRDS() behavior. ",
+      
+      warning("Nothing to overwrite - will use default saveRDS() behavior. ",
               "Additional arguments will be ignored!")
-
+      
       # if file does not exist (but archive is set to TRUE anyways),
       # save new file under specified name
       saveRDS(object = object, file = file, ...)
-
+      
     }
-
+    
   } else {
-
+    
     # OTHERWISE USE DEFAULT RDS -----------------------------------------------
-
-    warning("Nothing to overwrite - will use regular saveRDS() behavior. ",
+    
+    warning("'archive' is set to FALSE - will use default saveRDS() behavior. ",
             "Additional arguments will be ignored!")
-
+    
     saveRDS(object = object, file = file, ...)
-
+    
   }
-
+  
 }
