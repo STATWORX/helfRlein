5c4022a33d4f648ca50c914997ffad07245698d7
Jakob Gepp <jakob.gepp@statworx.de>
2020-08-10 10:38:21 +0200

    restructering print_fs

--- tests/testthat/test-print_fs.R
+++ tests/testthat/test-print_fs.R
@@ -20,3 +20,31 @@ test_that("path is positive numeric or integer", {
   expect_warning(print_fs(path = ".", depth = -2),
                  "depth was negative, set to 2")
 })
+
+test_that("output is correct", {
+
+  datapath <- system.file("test_filestructure", package = "helfRlein")
+
+  # null value
+  expect_null(print_fs(path = datapath))
+  # string vector
+  res_1 <- c("test_filestructure\n",
+             " |--folder_1\n",
+             " | o--file_1.txt\n",
+             " |--folder_2\n",
+             " | |--subfolder_1\n",
+             " | | |--file_211.txt\n",
+             " | | o--file_212.txt\n",
+             " | |--subfolder_2\n",
+             " | | |--file_221.txt\n",
+             " | | o--file_222.txt\n",
+             " | o--file_2.txt\n",
+             " |--folder_3\n",
+             " | o--file_3.txt\n",
+             " o--file_0.txt\n")
+  expect_equal(print_fs(path = datapath,
+                        return = TRUE,
+                        print = FALSE,
+                        depth = 4),
+               res_1)
+})
